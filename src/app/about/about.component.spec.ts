3ed550cbe962bba28db78ff9fc5b874f43c6c005
Stijn Aerts <stijn.aerts@bewire.be>
2016-06-29 14:39:59 +0200

    Tests added, manifest and shell added

---
+++ src/app/about/about.component.spec.ts
@@ -0,0 +1,15 @@
+/* tslint:disable:no-unused-variable */
+
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
+import { AboutComponent } from './about.component';
+
+beforeEachProviders(() => [AboutComponent]);
+
+describe('Page: about', () => {
+
+});
